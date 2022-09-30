c129ec1049dff977709aaf2ea1159b2039f91155
Michał Kukla <degregar@gmail.com>
2022-09-30 23:27:17 +0200

    docs: add basic swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle("DMN Engine Instance")
+    .setDescription("DMN Engine Instance")
+    .setVersion("1.0")
+    .addTag("dmn-engine")
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api", app, document)
+
+  await app.listen(3000)
 }
 
-void bootstrap();
+void bootstrap()
